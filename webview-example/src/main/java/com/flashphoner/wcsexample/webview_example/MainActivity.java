ab21386c2da20402cd997a33e6335b1c7dc005f6
Flashphoner <support@flashphoner.com>
2017-08-10 17:47:33 +0400

    [FIX] webview example minor issues.

--- webview-example/src/main/java/com/flashphoner/wcsexample/webview_example/MainActivity.java
+++ webview-example/src/main/java/com/flashphoner/wcsexample/webview_example/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
